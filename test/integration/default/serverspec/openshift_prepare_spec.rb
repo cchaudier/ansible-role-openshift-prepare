fc120831ed5bd895fd9da6db33dfaead4083bf75
Christophe Chaudier <devops@cchaudier.fr>
2017-01-11 15:43:55 +0100

    Add some tests

--- test/integration/default/serverspec/openshift_prepare_spec.rb
+++ test/integration/default/serverspec/openshift_prepare_spec.rb
@@ -3,9 +3,18 @@ require_relative 'spec_helper'
 packages = [ "wget", "git", "net-tools", "bind-utils", "iptables-services", "bridge-utils", "bash-completion", "ansible", "pyOpenSSL" ]
 
 packages.each do |package|
-  describe ""#{package} Packages" do
+  describe "#{package} Packages" do
       describe package("#{package}") do
           it { should be_installed }
       end
   end
+end
+
+describe yumrepo('epel') do
+  it { should exist }
+  it { should_not be_enabled }
+end
+
+describe selinux do
+  it { should_not be_disabled }
 end
